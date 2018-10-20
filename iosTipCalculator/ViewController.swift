802f1d491a37f759652ddf91fc3671cb25072fa1
peter <peter@peters-Mac-mini.local>
2018-10-19 17:01:49 -0700

    Initial Commit

---
+++ iosTipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iosTipCalculator
+//
+//  Created by peter on 10/19/18.
+//  Copyright © 2018 petecit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iosTipCalculatorTests/iosTipCalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  iosTipCalculatorTests.swift
+//  iosTipCalculatorTests
+//
+//  Created by peter on 10/19/18.
+//  Copyright © 2018 petecit. All rights reserved.
+//
+
+import XCTest
+@testable import iosTipCalculator
+
+class iosTipCalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
